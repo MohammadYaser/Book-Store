3762c31a7cd3feb6c896a29d3715b781f3de3769
MohammadYaser <yasersafi19@outlook.com>
2023-09-30 20:40:36 +0430

    Add style for the project

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,26 +12,38 @@ const Book = ({
   };
 
   return (
-    <li>
-      <div>
-        <span>Book title :</span>
-        {title}
+    <li className="book-card">
+      <div className="card-container">
+        <div className="book-container">
+          <h6>{category}</h6>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <ul className="btn-ul">
+            <li>
+              <button type="button" className="book-btn def">Comments</button>
+            </li>
+            <li>
+              <button type="button" className="book-btn" onClick={onRemoveClick}>Remove</button>
+            </li>
+            <li>
+              <button type="button" className="book-btn">Edit</button>
+            </li>
+          </ul>
+        </div>
+        <div className="circle-persantage">
+          <div className="circle" />
+          <div className="per">
+            <p className="persantage">100%</p>
+            <p className="completed">Completed</p>
+          </div>
+        </div>
+        <span className="boderline" />
+        <div className="chapter-container">
+          <h5>CURRENT CHAPTER</h5>
+          <p>Chapter 17</p>
+          <button type="button" className="update-btn">UPDATE PROGRESS</button>
+        </div>
       </div>
-      <div>
-
-        <span>Author :</span>
-        {author}
-
-      </div>
-      <div>
-
-        <span>Category :</span>
-        {category}
-
-      </div>
-      <button type="button" onClick={onRemoveClick}>
-        Remove
-      </button>
     </li>
   );
 };
